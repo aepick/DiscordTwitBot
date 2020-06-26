8ff365e7a59789c2fe9578f49118ffcf36374aa2
Alex Pickett <alexpickett@Alexs-MacBook-Pro.local>
2020-06-26 08:57:20 -0400

    key

--- k.js
+++
@@ -1,9 +0,0 @@
-const key = {
-    apiKey:'L7dYdydKvHQx9USPGxXJoR3py',
-    apiSecretKey:'XoiCn9tPjJfTskklqKrJsGPIqYpVkwHxWN4gSbzQ31txHiRFNp',
-    accessToken:'1275893363993194496-N5LJGpdK1m4XRzUZN6AgCtsaAgLmVV',
-    accessTokenSecret:'7UdPO5prDSl0Eldw6FZ24VBHI4l8arfiFiSJSElTXBddz',
-    botToken:'NzI1NDM3NzI0ODQ0MDk3NjE3.XvU9Bw.MF8o8g3w8PcGkL-7kKQ2JS0cJpo'
-}
-
-module.exports.key = key;
