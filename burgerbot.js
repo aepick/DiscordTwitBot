43270413bcaaa6d4cc9c3e5fe5023ef02266b55f
cluster3 <aepickwork@gmail.com>
2020-07-01 20:41:47 -0400

    updates

--- burgerbot.js
+++ burgerbot.js
@@ -4,12 +4,14 @@ const client = new Discord.Client();
 
 const fs = require("fs")
 // const key = require("./k.js").key;
-var Twit = require('twit')
+var Twit = require('twit');
+const { type } = require('os');
 var T = new Twit({
     consumer_key:         key.apiKey,
     consumer_secret:      key.apiSecretKey,
     access_token:         key.accessToken,
     access_token_secret:  key.accessTokenSecret,
+    tweet_mode:'extended'
 })
 
 
@@ -17,25 +19,6 @@ let users = JSON.parse(fs.readFileSync("./twitterID.json")).twitter
 console.log(users)
 // let users = ["104237736", "1275893363993194496", "20402945","203652149"];
 let stream = T.stream('statuses/filter', {follow: users});
-
-// const start = () => {
-//     // let data = JSON.parse(fs.readFileSync("./twitterID.json")).twitter
-//     // console.log(users)
-
-//     stream.on('tweet', function (tweet) {
-//         if (users.indexOf(tweet.user.id_str) > -1) {
-//             if(tweet.truncated == true) {
-//                 console.log(tweet.created_at + "\n" + tweet.user.name + ": " + tweet.extended_tweet.full_text);
-//             }
-//             else{
-//                 console.log(tweet.created_at + "\n" + tweet.user.name + ": " + tweet.text);
-//             }
-//             // console.log(tweet)
-//         }
-//     })
-    
-// }
-// start()
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -52,9 +35,16 @@ client.on('message', msg => {
 
 stream.on('tweet', function (tweet) {
     if (users.indexOf(tweet.user.id_str) > -1) {
-        if(tweet.truncated == true) {
+        if(typeof(tweet.retweeted_status) != "undefined" && typeof(tweet.retweeted_status.extended_tweet) != "undefined") {
+        
+            let dataT = `${tweet.created_at} \n${tweet.user.name}: ${ tweet.retweeted_status.extended_tweet.full_text}`
+            // console.log(tweet.extended_tweet.full_text)
+            client.channels.cache.get('725903080192868402').send(dataT)
+        }
+        else if(typeof(tweet.extended_tweet) != "undefined") {
             console.log(tweet.created_at + "\n" + tweet.user.name + ": " + tweet.extended_tweet.full_text);
             let dataT = `${tweet.created_at} \n${tweet.user.name}: ${tweet.extended_tweet.full_text}`
+            console.log(tweet.extended_tweet.full_text)
             client.channels.cache.get('725903080192868402').send(dataT)
 
         }
@@ -64,7 +54,7 @@ stream.on('tweet', function (tweet) {
             client.channels.cache.get('725903080192868402').send(dataT)
 
         }
-        // console.log(tweet)
+        console.log(tweet)
     }
 })
 
